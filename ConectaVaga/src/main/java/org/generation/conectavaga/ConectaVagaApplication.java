b26437fdc615ae53ece34fd8399143be00188e79
João Kiala Vioka Panzo <joaopanzo261@gmail.com>
2023-03-17 14:30:39 -0300

    Conecta Vaga Versão 1

---
+++ ConectaVaga/src/main/java/org/generation/conectavaga/ConectaVagaApplication.java
@@ -0,0 +1,13 @@
+package org.generation.conectavaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaVagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaVagaApplication.class, args);
+	}
+
+}
